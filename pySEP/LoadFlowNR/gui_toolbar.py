b3ec309b9010c194cfea37eff8da8998bb3e09e4
Marcos A Santos <mrcsantos1@outlook.com>
2020-06-12 11:46:52 -0300

    .

--- pySEP/LoadFlowNR/gui_toolbar.py
+++ pySEP/LoadFlowNR/gui_toolbar.py
@@ -4,18 +4,28 @@ import tkinter as tk
 def set_toolbar(janela_main):
     toolbar = tk.Frame(janela_main, bg="ghost white")
 
-    add_barra = tk.Button(toolbar, text="Barra", command=add_bar)
+    ## Adicionar Barra
+
+    add_barra = tk.Button(toolbar, text="Barra",
+                          relief=tk.FLAT)
+    # command=add_bar)
+    add_barra.bind("<Button-1>", add_bar)
+
     add_barra.pack(side=tk.LEFT, padx=2, pady=2)
 
-    add_linha = tk.Button(toolbar, text="Linha", command=add_lin)
+    ## Adicionar Linha
+
+    add_linha = tk.Button(toolbar, text="Linha",
+                          relief=tk.FLAT)
+    add_linha.bind("<Button-1>", add_lin)
     add_linha.pack(side=tk.LEFT, padx=2, pady=2)
 
     toolbar.pack(side=tk.TOP, fill=tk.X)
 
 
-def add_bar():
-    print('\nAdicionar barra!')
+def add_bar(event):
+    print('\nAdicionar barra!', event)
 
 
-def add_lin():
-    print('\nAdicionar linha!')
+def add_lin(event):
+    print('\nAdicionar linha!', event)
