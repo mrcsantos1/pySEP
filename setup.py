30d144c678e87fb0cd1c15394ffea06a9d4c3365
Marcos A Santos <mrcsantos1@outlook.com>
2020-06-18 16:32:38 -0300

    1.0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 setup(
     name='pySEP',
-    version='1.0.7',
+    version='1.0.8',
     author='Marcos Alves dos Santos',
     author_email='mrcsantos1@outlook.com',
     packages=['pySEP'],
